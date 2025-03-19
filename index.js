d99c3bf15cda66000e7655231d8b500954ec50ff
Douglas Bastos <dautodouglasdauto@gmail.com>
2025-03-19 17:41:09 -0300

    Finalizado o projeto

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ function update() {
     let billEach = billTotal / split;
 
     document.getElementById("tipPercent").innerHTML = `${tipPercent}%`;
-    document.getElementById("tipValue").innerHTML = `$${tipValue}`;
+    document.getElementById("tipValue").innerHTML = formatMoney(tipValue, 2);
+    document.getElementById("totalWithTip").innerHTML = formatMoney(billTotal, 2);
+    document.getElementById("splitValue").innerHTML = formatSplit(split);
+    document.getElementById("billEach").innerHTML = formatMoney(billTotal / split, 2);
+}
+
+function formatMoney(value, qtdDecimal) {
+    return `$${Math.ceil(Number((value * 100) / 100).toFixed(Number(qtdDecimal)))}`;
+}
+
+function formatSplit(value) {
+    return Number(value) === 1?`${value} person`:`${value} people`
 }
